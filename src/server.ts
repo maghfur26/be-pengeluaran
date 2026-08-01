611cd31b71957c5bbbe81d6adb6f73a4df442916
maghfur26 <maghfurhasani9@gmail.com>
2026-08-01 18:53:57 +0700

    fix conflic cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './config/database';
 import expenseRoutes from './routes/expenseRoutes';
 
 dotenv.config();
@@ -17,6 +18,18 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
+
+app.use(async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Gagal terhubung ke database. Pastikan MONGODB_URI sudah diset.'
+    });
+  }
+});
 
 app.use('/api/expenses', expenseRoutes);
 app.use('/expenses', expenseRoutes);
